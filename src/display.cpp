5c5fe93283224e782f65598050e33b88eab8652b
smillier <sylvain.millier@gmail.com>
2022-05-01 16:55:55 +0200

    Weight measure

--- src/display.cpp
+++ src/display.cpp
@@ -32,20 +32,25 @@ static unsigned long targetTime; // next action time
 static int16_t *cached_points;
 static uint16_t cached_points_idx = 0;
 static int16_t *last_cached_point;
-
+int keg_level = 0;
 #include "BmpClass.h"
 static BmpClass bmpClass;
+
+// Meter colour schemes
+#define RED2RED 0
+#define GREEN2GREEN 1
+#define BLUE2BLUE 2
+#define BLUE2RED 3
+#define GREEN2RED 4
+#define RED2GREEN 5
 
 void display_init()
 {
      gfx->begin();
     gfx->fillScreen(BACKGROUND);
     Serial.begin(9600);
-   
-  if(!SPIFFS.begin()){
-        Serial.println("SPIFFS Mount Failed");
-        return;
-    }
+    
+  
 #ifdef TFT_BL
     pinMode(TFT_BL, OUTPUT);
     digitalWrite(TFT_BL, HIGH);
@@ -76,6 +81,44 @@ void display_init()
         center - (markLen / 2), center);
 }
 
+// #########################################################################
+// Return a 16 bit rainbow colour
+// #########################################################################
+unsigned int rainbow(uint8_t value)
+{
+  // Value is expected to be in range 0-127
+  // The value is converted to a spectrum colour from 0 = blue through to 127 = red
+
+  uint8_t red = 0; // Red is the top 5 bits of a 16 bit colour value
+  uint8_t g = 0;// Green is the middle 6 bits
+  uint8_t b = 0; // Blue is the bottom 5 bits
+
+  uint8_t quadrant = value / 32;
+
+  if (quadrant == 0) {
+    b = 31;
+    g = 2 * (value % 32);
+    red = 0;
+  }
+  if (quadrant == 1) {
+    b = 31 - (value % 32);
+    g = 63;
+    red = 0;
+  }
+  if (quadrant == 2) {
+    b = 0;
+    g = 63;
+    red = value % 32;
+  }
+  if (quadrant == 3) {
+    b = 0;
+    g = 63 - 2 * (value % 32);
+    red = 31;
+  }
+  return (red << 11) + (g << 5) + b;
+}
+
+
 // pixel drawing callback
 static void bmpDrawCallback(int16_t x, int16_t y, uint16_t *bitmap, int16_t w, int16_t h)
 {
@@ -85,67 +128,110 @@ static void bmpDrawCallback(int16_t x, int16_t y, uint16_t *bitmap, int16_t w, i
 
 void draw_round_clock_mark(int16_t innerR1, int16_t outerR1, int16_t innerR2, int16_t outerR2, int16_t innerR3, int16_t outerR3)
 {
-  float x, y;
-  int16_t x0, x1, y0, y1, innerR, outerR;
-  uint16_t c;
-  float mdeg2;
+  
   File bmpFile = SPIFFS.open("/NSRM12.bmp", "r");
   // read JPEG file header
+    
+    
+    gfx->setCursor(50, 120);
+    gfx->setTextSize(2 /* x scale */, 2 /* y scale */, 2);
+    gfx->setTextColor(WHITE);
+    gfx->println("Red Irish IPA");
+    gfx->setCursor(70, 150);
+    gfx->setTextSize(2 /* x scale */, 2 /* y scale */, 2);
+    gfx->setTextColor(WHITE);
+    gfx->println("IBU: 57");
+    gfx->setCursor(70, 170);
+    gfx->setTextSize(2 /* x scale */, 2 /* y scale */, 2);
+    gfx->setTextColor(WHITE);
+    gfx->println("ABV: 6.5%");
+    gfx->setCursor(50, 190);
+    gfx->setTextSize(2 /* x scale */, 2 /* y scale */, 2);
+    gfx->setTextColor(WHITE);
+    //Get Keg weight
+    float kegWeight  = getWeight();
+    int valueToPrint = (100/0.33) * kegWeight;
+    gfx->println("Keg: " + String(kegWeight) + "Kg ");
+    // Set the the position, gap between meters, and inner radius of the meters
+    int xpos = 0, ypos = 5, gap = 4, radius = 120;
+
+    // Draw meter and get back x position of next meter
+
+    
+    
+    xpos = gap + ringMeter(valueToPrint, 0, 100, xpos, ypos, radius, "mA", RED2GREEN); // Draw analogue meter
+
     bmpClass.draw(
         &bmpFile, bmpDrawCallback, false /* useBigEndian */,
         0 /* x */, -50 /* y */, gfx->width() /* widthLimit */, gfx->height() /* heightLimit */);
 
     bmpFile.close();
+    
+}
 
-  for (uint8_t i = 0; i < 60; i++)
-  {
-    
-      innerR = innerR2;
-      outerR = outerR2;
-      if (i< 20)
-      {
-        c=HOP_COLOR;
-      }
-      else
-      {
-       c = MARK_COLOR;
-      }
+// #########################################################################
+//  Draw the meter on the screen, returns x coord of righthand side
+// #########################################################################
+int ringMeter(int value, int vmin, int vmax, int x, int y, int r, char *units, uint8_t scheme)
+{
+  // Minimum value of r is about 52 before value text intrudes on ring
+  // drawing the text first is an option
+  
+  x += r; y += r;   // Calculate coords of centre of ring
 
-    mdeg = (SIXTIETH_RADIAN * i) +2.8;//+ RIGHT_ANGLE_RADIAN;
-    mdeg2=(SIXTIETH_RADIAN * i);
-    x = cos(mdeg);
-    y = sin(mdeg);
-    x0 = x * outerR + center;
-    y0 = y * outerR + center;
-    x1 = x * innerR + center;
-    y1 = y * innerR + center;
-    
-    
-    
-    if (mdeg2 <= LEFT_STOP)
+  int w = r / 8;    // Width of outer ring is 1/4 of radius
+  
+  int angle = 150;  // Half the sweep angle of meter (300 degrees)
+
+  int text_colour = 5; // To hold the text colour
+
+  int v = map(value, vmin, vmax, -angle, angle); // Map the value to an angle v
+
+  byte seg = 5; // Segments are 5 degrees wide = 60 segments for 300 degrees
+  byte inc = 5; // Draw segments every 5 degrees, increase to 10 for segmented ring
+
+  // Draw colour blocks every inc degrees
+  for (int i = -angle; i < angle; i += inc) {
+
+    // Choose colour from scheme
+    int colour = 0;
+    switch (scheme) {
+      case 0: colour = 1; break; // Fixed colour
+      case 1: colour = 2; break; // Fixed colour
+      case 2: colour = 3; break; // Fixed colour
+      case 3: colour = rainbow(map(i, -angle, angle, 0, 127)); break; // Full spectrum blue to red
+      case 4: colour = rainbow(map(i, -angle, angle, 63, 127)); break; // Green to red (high temperature etc)
+      case 5: colour = rainbow(map(i, -angle, angle, 127, 63)); break; // Red to green (low battery etc)
+      default: colour = 2; break; // Fixed colour
+    }
+
+    // Calculate pair of coordinates for segment start
+    float sx = cos((i - 90) * 0.0174532925);
+    float sy = sin((i - 90) * 0.0174532925);
+    uint16_t x0 = sx * (r - w) + x;
+    uint16_t y0 = sy * (r - w) + y;
+    uint16_t x1 = sx * r + x;
+    uint16_t y1 = sy * r + y;
+
+    // Calculate pair of coordinates for segment end
+    float sx2 = cos((i + seg - 90) * 0.0174532925);
+    float sy2 = sin((i + seg - 90) * 0.0174532925);
+    int x2 = sx2 * (r - w) + x;
+    int y2 = sy2 * (r - w) + y;
+    int x3 = sx2 * r + x;
+    int y3 = sy2 * r + y;
+
+    if (i < v) { // Fill in coloured segments with 2 triangles
+      gfx->fillTriangle(x0, y0, x1, y1, x2, y2, colour);
+      gfx->fillTriangle(x1, y1, x2, y2, x3, y3, colour);
+      text_colour = colour; // Save the last colour drawn
+    }
+    else // Fill in blank segments
     {
-      gfx->drawLine(x0, y0, x1, y1, c);
+      gfx->fillTriangle(x0, y0, x1, y1, x2, y2, 0);
+      gfx->fillTriangle(x1, y1, x2, y2, x3, y3, 0);
     }
   }
-    
-    gfx->setCursor(10, 120);
-    gfx->setTextSize(2 /* x scale */, 2 /* y scale */, 2);
-    gfx->setTextColor(WHITE);
-    gfx->println("Red Irish IPA");
-    gfx->setCursor(40, 150);
-    gfx->setTextSize(2 /* x scale */, 2 /* y scale */, 2);
-    gfx->setTextColor(WHITE);
-    gfx->println("IBU: 57");
-    gfx->setCursor(40, 170);
-    gfx->setTextSize(2 /* x scale */, 2 /* y scale */, 2);
-    gfx->setTextColor(WHITE);
-    gfx->println("ABV: 6.5%");
-    float kegWeight  = getWeight();
-    gfx->setCursor(40, 190);
-    gfx->setTextSize(2 /* x scale */, 2 /* y scale */, 2);
-    gfx->setTextColor(WHITE);
-    gfx->println("Keg: " + String(kegWeight/1000) + "Kg");
-    
 }
 
 void write_cache_pixel(int16_t x, int16_t y, int16_t color, bool cross_check_second, bool cross_check_hour)
@@ -179,7 +265,7 @@ void write_cache_pixel(int16_t x, int16_t y, int16_t color, bool cross_check_sec
 
 void draw_and_erase_cached_line(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t color, int16_t *cache, int16_t cache_len, bool cross_check_second, bool cross_check_hour)
 {
-#if defined(ESP8266)
+       #if defined(ESP8266)
     yield();
 #endif
     bool steep = _diff(y1, y0) > _diff(x1, x0);
@@ -248,6 +334,7 @@ void draw_and_erase_cached_line(int16_t x0, int16_t y0, int16_t x1, int16_t y1, 
         *(cache + (i * 2)) = 0;
         *(cache + (i * 2) + 1) = 0;
     }
+  
 }
 
 
@@ -256,7 +343,8 @@ void redraw_hands_cached_draw_and_erase()
 {
     gfx->startWrite();
     draw_and_erase_cached_line(center, center, nsx, nsy, SECOND_COLOR, cached_points, sHandLen + 1, false, false);
-    draw_and_erase_cached_line(center, center, nhx, nhy, HOUR_COLOR, cached_points + ((sHandLen + 1) * 2), hHandLen + 1, true, false);
-    draw_and_erase_cached_line(center, center, nmx, nmy, MINUTE_COLOR, cached_points + ((sHandLen + 1 + hHandLen + 1) * 2), mHandLen + 1, true, true);
+   // draw_and_erase_cached_line(center, center, nhx, nhy, HOUR_COLOR, cached_points + ((sHandLen + 1) * 2), hHandLen + 1, true, false);
+   // draw_and_erase_cached_line(center, center, nmx, nmy, MINUTE_COLOR, cached_points + ((sHandLen + 1 + hHandLen + 1) * 2), mHandLen + 1, true, true);
     gfx->endWrite();
 }
+
